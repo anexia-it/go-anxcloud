ee242c96c0a1eb125e384d327ce11faade62b213
Mara Sophie Grosch <mgrosch@anexia-it.com>
2021-11-22 12:19:44 +0100

    🎨: extract object channel run method

--- pkg/api/object_channel.go
+++ pkg/api/object_channel.go
@@ -42,46 +42,7 @@ func (oc *objectChannel) Channel() (types.ObjectChannel, error) {
 	}
 
 	pageRetrieveWaiter := make(chan bool)
-
-	go func() {
-		var pageData []json.RawMessage
-
-	cancelChannel:
-		for oc.pageIterator.Next(&pageData) {
-			for _, o := range pageData {
-				select {
-				case <-oc.abort:
-					oc.log.Info("User requested aborting the channel")
-					break cancelChannel
-				case <-pageRetrieveWaiter:
-					oc.log.Info("Object retrieved by user")
-				}
-
-				oc.log.Info("Pushing retriever on channel")
-
-				// since we are in a goroutine, we might already be in the next iteration of this loop
-				// at the time the receiving end of this channel calls the closure. Having a loop-body
-				// scoped variables makes the data for the closure perfectly identified.
-				closureData := o
-				oc.objects <- func(out types.Object) error {
-					err := decodeResponse(oc.ctx, "application/json", bytes.NewBuffer(closureData), out)
-					if err != nil {
-						return err
-					}
-
-					pageRetrieveWaiter <- true
-					return nil
-				}
-			}
-
-			oc.log.Info("Retrieving next page")
-		}
-
-		// we have to wait for last retriever used before closing the channel
-		<-pageRetrieveWaiter
-		close(oc.objects)
-	}()
-
+	go oc.run(pageRetrieveWaiter)
 	pageRetrieveWaiter <- true
 
 	return types.ObjectChannel(oc.objects), nil
@@ -119,4 +80,43 @@ func (oc *objectChannel) prepare(ctx context.Context, api API, opts *types.ListO
 	opts.PageInfo = &oc.pageIterator
 
 	return nil
+}
+
+func (oc *objectChannel) run(pageRetrieveWaiter chan bool) {
+	var pageData []json.RawMessage
+
+cancelChannel:
+	for oc.pageIterator.Next(&pageData) {
+		for _, o := range pageData {
+			select {
+			case <-oc.abort:
+				oc.log.Info("User requested aborting the channel")
+				break cancelChannel
+			case <-pageRetrieveWaiter:
+				oc.log.Info("Object retrieved by user")
+			}
+
+			oc.log.Info("Pushing retriever on channel")
+
+			// since we are in a goroutine, we might already be in the next iteration of this loop
+			// at the time the receiving end of this channel calls the closure. Having a loop-body
+			// scoped variables makes the data for the closure perfectly identified.
+			closureData := o
+			oc.objects <- func(out types.Object) error {
+				err := decodeResponse(oc.ctx, "application/json", bytes.NewBuffer(closureData), out)
+				if err != nil {
+					return err
+				}
+
+				pageRetrieveWaiter <- true
+				return nil
+			}
+		}
+
+		oc.log.Info("Retrieving next page")
+	}
+
+	// we have to wait for last retriever used before closing the channel
+	<-pageRetrieveWaiter
+	close(oc.objects)
 }
