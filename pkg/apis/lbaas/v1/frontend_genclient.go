7747cf6d4a142209c1ae752472b9e14572a07f17
Mara Sophie Grosch <mgrosch@anexia-it.com>
2021-11-30 11:46:01 +0100

    ✨: add LBaaS Frontends filters LoadBalancer+Mode
    We can now List Frontends with the generic client with filters for
    LoadBalancer and Mode.

--- pkg/apis/lbaas/v1/frontend_genclient.go
+++ pkg/apis/lbaas/v1/frontend_genclient.go
@@ -10,8 +10,30 @@ import (
 // EndpointURL returns the URL where to retrieve objects of type Frontend and the identifier of the given Frontend.
 // It implements the api.Object interface on *Frontend, making it usable with the generic API client.
 func (f *Frontend) EndpointURL(ctx context.Context) (*url.URL, error) {
-	url, err := url.ParseRequestURI("/api/LBaaS/v1/frontend.json")
-	return url, err
+	op, err := types.OperationFromContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	u, err := url.ParseRequestURI("/api/LBaaS/v1/frontend.json")
+
+	if op == types.OperationList {
+		filters := make(url.Values)
+
+		if f.LoadBalancer.Identifier != "" {
+			filters.Add("load_balancer", f.LoadBalancer.Identifier)
+		}
+
+		if f.Mode != "" {
+			filters.Add("mode", string(f.Mode))
+		}
+
+		query := u.Query()
+		query.Add("filters", filters.Encode())
+		u.RawQuery = query.Encode()
+	}
+
+	return u, err
 }
 
 // FilterAPIRequestBody generates the request body for creating a new Frontend, which differs from the Frontend object.
