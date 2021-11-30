237f43bc3f9afe34a6742759e15b58e90dcda43b
Mara Sophie Grosch <mgrosch@anexia-it.com>
2021-11-30 11:32:01 +0100

    👽️💩 LBaas: add rules and state for front-/backend
    Extracts LoadBalancerState from LoadBalancer types as StateObject to common
    types, moves RuleInfo from LoadBalancer types to common types and uses those
    for Frontend and Backend, too.
    
    Also fixes Frontend not having an attribute tagged with anxcloud:"identifier".

--- pkg/apis/lbaas/v1/types.go
+++ pkg/apis/lbaas/v1/types.go
@@ -16,3 +16,20 @@ const (
 	Deployed        = State("3")
 	NewlyCreated    = State("4")
 )
+
+// StateObject describes the status of a given resource, including programatic usable and human readable values.
+type StateObject struct {
+	// programatically usable enum value
+	ID State `json:"id"`
+
+	// human readable status text
+	Text string `json:"text"`
+
+	Type int `json:"type"`
+}
+
+// RuleInfo holds the name and identifier of a rule.
+type RuleInfo struct {
+	Identifier string `json:"identifier" anxcloud:"identifier"`
+	Name       string `json:"name"`
+}
