a346207bb47ad6822754784650c43547db83af2b
Mara Sophie Grosch <mgrosch@anexia-it.com>
2021-12-08 16:19:39 +0100

    ♻️🚚: move VLAN tests into their packages

---
+++ pkg/vlan/integration_test.go
@@ -0,0 +1,115 @@
+// +build integration
+// go:build integration
+
+package vlan
+
+import (
+	"context"
+	"time"
+
+	"github.com/anexia-it/go-anxcloud/pkg/client"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+const (
+	locationID = "52b5f6b2fd3a4a7eaaedf1a7c019e9ea"
+)
+
+var _ = Describe("vlan client", func() {
+	var cli client.Client
+	var api API
+
+	BeforeEach(func() {
+		var err error
+		cli, err = client.New(client.AuthFromEnv(false))
+		Expect(err).ToNot(HaveOccurred())
+
+		api = NewAPI(cli)
+	})
+
+	Context("with a VLAN created for testing", Ordered, func() {
+		var vlanID string
+
+		BeforeAll(func() {
+			def := CreateDefinition{
+				Location:            locationID,
+				VMProvisioning:      false,
+				CustomerDescription: "go SDK integration test",
+			}
+			summary, err := api.Create(context.TODO(), def)
+			Expect(err).NotTo(HaveOccurred())
+
+			DeferCleanup(func() {
+				err := api.Delete(context.TODO(), summary.Identifier)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			vlanID = summary.Identifier
+		})
+
+		It("lists VLANs including test VLAN", func() {
+			found := false
+			page := 1
+
+			for !found {
+				vs, err := api.List(context.TODO(), page, 20, "")
+				Expect(err).To(BeNil())
+				Expect(vs).NotTo(BeEmpty())
+
+				for _, v := range vs {
+					if v.Identifier == vlanID {
+						found = true
+						break
+					}
+				}
+
+				page++
+			}
+		})
+
+		It("eventually retrieves test VLAN with expected data and being active", func() {
+			pollCheck := func(g Gomega) {
+				vlan, err := api.Get(context.TODO(), vlanID)
+				g.Expect(err).NotTo(HaveOccurred())
+
+				g.Expect(vlan.Locations).To(HaveLen(1))
+				g.Expect(vlan.Locations[0].Identifier).To(Equal(locationID))
+
+				g.Expect(vlan.VMProvisioning).To(BeFalse())
+				g.Expect(vlan.CustomerDescription).To(Equal("go SDK integration test"))
+
+				g.Expect(vlan.Status).To(Equal("Active"))
+			}
+
+			Eventually(pollCheck, 5*time.Minute, 10*time.Second).Should(Succeed())
+		})
+
+		It("updates test VLAN with changed data", func() {
+			def := UpdateDefinition{
+				CustomerDescription: "go SDK integration test updated",
+				VMProvisioning:      true,
+			}
+
+			err := api.Update(context.TODO(), vlanID, def)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("eventually retrieves test VLAN with expected updated data", func() {
+			pollCheck := func(g Gomega) {
+				vlan, err := api.Get(context.TODO(), vlanID)
+				g.Expect(err).NotTo(HaveOccurred())
+
+				g.Expect(vlan.Locations).To(HaveLen(1))
+				g.Expect(vlan.Locations[0].Identifier).To(Equal(locationID))
+
+				g.Expect(vlan.VMProvisioning).To(BeTrue())
+				g.Expect(vlan.CustomerDescription).To(Equal("go SDK integration test updated"))
+				g.Expect(vlan.Status).To(Equal("Active"))
+			}
+
+			Eventually(pollCheck, 5*time.Minute, 10*time.Second).Should(Succeed())
+		})
+	})
+})
---
+++ pkg/vlan/vlan_test.go
@@ -0,0 +1,13 @@
+package vlan
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestSpecRunner(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "vlan tests")
+}
