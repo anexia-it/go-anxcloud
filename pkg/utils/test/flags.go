cd9a5930c525f32f0cd17f8a0ffef231a8c074d4
Mara Sophie Grosch <mgrosch@anexia-it.com>
2021-12-09 13:02:55 +0100

    Remove test flags from pkg/utils/test

--- pkg/utils/test/flags.go
+++
@@ -1,9 +0,0 @@
-package test
-
-import "flag"
-
-var RunAsIntegrationTest = false
-
-func InitFlags() {
-	flag.BoolVar(&RunAsIntegrationTest, "integration-test", RunAsIntegrationTest, "Run the test suite as integration tests against the real Engine API")
-}
