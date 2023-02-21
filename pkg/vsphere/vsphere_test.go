ba80fe3a36f0582b7301af348eaa82300e3563cb
Mara Sophie Grosch <mgrosch@anexia-it.com>
2023-02-21 14:36:46 +0100

    Fix vsphere integration tests
    Were broken by 08d2699bdedce9a61cfc3c18fd5cde427c8c8237

--- pkg/vsphere/vsphere_test.go
+++ pkg/vsphere/vsphere_test.go
@@ -3,11 +3,15 @@ package vsphere
 import (
 	"testing"
 
+	testutil "go.anx.io/go-anxcloud/pkg/utils/test"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
 func TestSpecRunner(t *testing.T) {
+	testutil.Seed(GinkgoRandomSeed())
+
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "vsphere tests")
 }
