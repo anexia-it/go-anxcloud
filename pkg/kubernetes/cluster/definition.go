a0e7aae3fdc53a3eaa660a2a9e47b8b7a6e66314
David Kröll <dkroell@anexia-it.com>
2025-11-20 14:49:36 +0100

    make bools to ints

--- pkg/kubernetes/cluster/definition.go
+++ pkg/kubernetes/cluster/definition.go
@@ -11,24 +11,31 @@ const (
 	LocationANX25 = Location("97b3cdf37368470496a249aff0b84845")
 )
 
+type IntBoolean int
+
+const (
+	Disable IntBoolean = 0
+	Enable  IntBoolean = 1
+)
+
 type Definition struct {
-	Name                     string   `json:"name"`
-	State                    gs.State `json:"state"`
-	Location                 Location `json:"location"`
-	Version                  string   `json:"version"`
-	Autoscaling              bool     `json:"autoscaling"`
-	CNIPlugin                string   `json:"cni_plugin"`
-	APIServerAllowList       string   `json:"api_server_allow_list"`
-	MaintenanceWindowStart   string   `json:"maintenance_window_start"`
-	MaintenanceWindowLength  string   `json:"maintenance_window_length"`
-	ManageInternalIPv4Prefix bool     `json:"manage_internal_ipv4_prefix"`
-	InternalIPv4Prefix       string   `json:"internal_ipv4_prefix"`
-	NeedsServiceVMs          bool     `json:"needs_service_vms"`
-	EnableNATGateways        bool     `json:"enable_nat_gateways"`
-	EnableLBaaS              bool     `json:"enable_lbaas"`
-	ExternalIPFamilies       string   `json:"external_ip_families"`
-	ManageExternalIPv4Prefix bool     `json:"manage_external_ipv4_prefix"`
-	ExternalIPv4Prefix       string   `json:"external_ipv4_prefix"`
-	ManageExternalIPv6Prefix bool     `json:"manage_external_ipv6_prefix"`
-	ExternalIPv6Prefix       string   `json:"external_ipv6_prefix"`
+	Name                     string     `json:"name"`
+	State                    gs.State   `json:"state"`
+	Location                 Location   `json:"location"`
+	Version                  string     `json:"version"`
+	Autoscaling              IntBoolean `json:"autoscaling"`
+	CNIPlugin                string     `json:"cni_plugin"`
+	APIServerAllowList       string     `json:"api_server_allow_list"`
+	MaintenanceWindowStart   string     `json:"maintenance_window_start"`
+	MaintenanceWindowLength  string     `json:"maintenance_window_length"`
+	ManageInternalIPv4Prefix IntBoolean `json:"manage_internal_ipv4_prefix"`
+	InternalIPv4Prefix       string     `json:"internal_ipv4_prefix"`
+	NeedsServiceVMs          IntBoolean `json:"needs_service_vms"`
+	EnableNATGateways        IntBoolean `json:"enable_nat_gateways"`
+	EnableLBaaS              IntBoolean `json:"enable_lbaas"`
+	ExternalIPFamilies       string     `json:"external_ip_families"`
+	ManageExternalIPv4Prefix IntBoolean `json:"manage_external_ipv4_prefix"`
+	ExternalIPv4Prefix       string     `json:"external_ipv4_prefix"`
+	ManageExternalIPv6Prefix IntBoolean `json:"manage_external_ipv6_prefix"`
+	ExternalIPv6Prefix       string     `json:"external_ipv6_prefix"`
 }
