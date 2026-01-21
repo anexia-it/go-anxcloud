38f9ae293af3a17b77a9968e31c7f0f12da6bfab
David Kröll <dkroell@anexia-it.com>
2026-01-21 14:57:53 +0100

    add nodepool to collection

--- pkg/kubernetes/api.go
+++ pkg/kubernetes/api.go
@@ -3,26 +3,35 @@
 package kubernetes
 
 import (
+	"go.anx.io/go-anxcloud/pkg/apis/common"
 	"go.anx.io/go-anxcloud/pkg/client"
 	"go.anx.io/go-anxcloud/pkg/kubernetes/cluster"
+	"go.anx.io/go-anxcloud/pkg/kubernetes/nodepool"
 )
 
 // API contains methods for IP manipulation.
 type API interface {
 	Cluster() cluster.API
+	Nodepool() nodepool.API
 }
 
 type api struct {
-	cluster cluster.API
+	cluster  cluster.API
+	nodepool nodepool.API
 }
 
 func (a api) Cluster() cluster.API {
 	return a.cluster
 }
 
+func (a api) Nodepool() nodepool.API {
+	return a.nodepool
+}
+
 // NewAPI creates a new IP API instance with the given client.
-func NewAPI(c client.Client) API {
+func NewAPI(c client.Client, opts common.ClientOpts) API {
 	return &api{
-		cluster.NewAPI(c),
+		cluster:  cluster.NewAPI(c, opts),
+		nodepool: nodepool.NewAPI(c, opts),
 	}
 }
