4213ec04c39f16965951f6bb53e26e8135d14bed
dbs5 <ddunz@anexia-it.com>
2026-04-01 17:04:33 +0200

    fix linter error 'naked return'

--- pkg/vsphere/provisioning/availabilityzones/api.go
+++ pkg/vsphere/provisioning/availabilityzones/api.go
@@ -31,7 +31,7 @@ func NewAPI(c client.Client) API {
 }
 
 func (a api) List(ctx context.Context, locationID string) (
-	zones []AvailabilityZone, err error) {
+	[]AvailabilityZone, error) {
 
 	url := fmt.Sprintf(
 		"%s/api/vsphere/v1/provisioning/location.json/%s/availability_zone",
@@ -42,25 +42,26 @@ func (a api) List(ctx context.Context, locationID string) (
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		err = fmt.Errorf("could not create ListAvailabilityZones request: %w", err)
-		return
+		return nil, err
 	}
 
 	httpResponse, err := a.client.Do(req)
 	if err != nil {
 		err = fmt.Errorf("could not execute ListAvailabilityZones request: %w", err)
-		return
+		return nil, err
 	}
 	defer httpResponse.Body.Close()
 	if httpResponse.StatusCode != 200 {
 		err = fmt.Errorf("could not execute ListAvailabilityZones request, got response %s", httpResponse.Status)
-		return
+		return nil, err
 	}
 
+	var zones []AvailabilityZone
 	err = json.NewDecoder(httpResponse.Body).Decode(&zones)
 	if err != nil {
 		err = fmt.Errorf("could not decode ListAvailabilityZones response: %w", err)
-		return
+		return nil, err
 	}
 
-	return
+	return nil, err
 }
